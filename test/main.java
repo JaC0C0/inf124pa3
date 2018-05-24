aca79a17921db53eeb9aaa089e4171c30312eeeb
Dennis Tian <JaCoCo@localhost.localdomain>
2018-05-24 01:51:23 -0700

    uncommented out code, restarted server

--- test/main.java
+++ test/main.java
@@ -9,31 +9,30 @@ import javax.servlet.http.*;
 
 public class main extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-//        String url = "jdbc:mysql:@matt-smith-v4.ics.uci.edu";
-//        String user = "inf124db057";
-//        String password = "wRd8MJP2XGWa";
-//
-//        String sql = "SELECT * FROM `PRODUCTS` WHERE pid = 432231";
-//        response.setContentType("text/html");
-//        PrintWriter out = response.getWriter();
-//        try {
-//    //            DriverManager.registerDriver(new org.gjt.mm.mysql.Driver());
-//            Class.forName ("org.gjt.mm.mysql.Driver");
-//            Connection conn = DriverManager.getConnection(url, user, password);
-//            Statement st = conn.createStatement();
-//            int m = st.executeUpdate(sql);
-//            if (m == 1)
-//                out.println("inserted successfully : "+sql);
-//            else
-//                out.println("insertion failed");
-//            conn.close();
-//        } catch (Exception e) {
-//            out.println(e);
-//        }
+        String url = "jdbc:mysql:@matt-smith-v4.ics.uci.edu";
+        String user = "inf124db057";
+        String password = "wRd8MJP2XGWa";
+
+        String sql = "SELECT * FROM `PRODUCTS` WHERE pid = 432231";
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        try {
+    //            DriverManager.registerDriver(new org.gjt.mm.mysql.Driver());
+            Class.forName ("org.gjt.mm.mysql.Driver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement st = conn.createStatement();
+            int m = st.executeUpdate(sql);
+            if (m == 1)
+                out.println("inserted successfully : "+sql);
+            else
+                out.println("insertion failed");
+            conn.close();
+        } catch (Exception e) {
+            out.println(e);
+        }
     }
 
     public void destroy() {
 
     }
-
 }
