567fd152c353d5d975e92b0e6c28ab6921b5853a
Dennis Tian <JaCoCo@localhost.localdomain>
2018-05-24 01:08:02 -0700

    changed import statements to reflect test class from HelloWorld

--- test/main.java
+++ test/main.java
@@ -1,10 +1,11 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
 import java.sql.*;
-import java.util.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 public class main extends HttpServlet{
 //    public static void main() {
