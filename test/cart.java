c7e2d34f9112315a4f6bf2dd64bf87cbda826fc8
k-m-h <kenth1@uci.edu>
2018-05-29 08:38:58 -0700

    urlencoded include link

--- test/cart.java
+++ test/cart.java
@@ -6,6 +6,7 @@ import javax.servlet.http.*;
 import com.mysql.jdbc.Driver;
 import java.sql.*;
 import java.util.ArrayList;
+import java.net.URLEncoder;
 
 public class cart extends HttpServlet{
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
@@ -31,7 +32,7 @@ public class cart extends HttpServlet{
         response.setContentType("text/html");
 
         try {
-            RequestDispatcher rd = request.getRequestDispatcher("item?pid=" + request.getParameter("pid"));
+            RequestDispatcher rd = request.getRequestDispatcher("item?pid=" + URLEncoder.encode(request.getParameter("pid"), "UTF-8"));
             rd.include(request, response);
             out.println("    <h4>Item Added to Cart</h4>\n");
         } catch (Exception e) {
