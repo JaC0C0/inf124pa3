7fcafb92ee737d443a081c27598528b6cf7a0c01
Dennis Tian <JaCoCo@localhost.localdomain>
2018-05-29 08:55:11 -0700

    added purchase and stuff

---
+++ test/purchase.java
@@ -0,0 +1,17 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class purchase extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String url = "jdbc:mysql://matt-smith-v4.ics.uci.edu/inf124db057?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String user = "inf124db057";
+        String password = "wRd8MJP2XGWa";
+        HttpSession session = request.getSession();
+        ArrayList<ArrayList<Integer>> cart = (ArrayList<ArrayList<Integer>>)session.getAttribute("cart");
+
+    }
+}
