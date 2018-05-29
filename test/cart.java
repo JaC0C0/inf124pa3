bafebb674fa7a130057162c63e7ba625b73d7ab3
k-m-h <kenth1@uci.edu>
2018-05-29 07:49:18 -0700

    completed cart include

--- test/cart.java
+++ test/cart.java
@@ -5,11 +5,22 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import com.mysql.jdbc.Driver;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class cart extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
         HttpSession session = request.getSession();
-        session.setAttribute("pid",request.getParameter("pid"));
+        ArrayList<ArrayList<Integer>> cart = new ArrayList<ArrayList<Integer>>();
+
+        if (session.getAttribute("cart") != null) {
+          cart = (ArrayList<ArrayList<Integer>>)session.getAttribute("cart");
+        }
+        ArrayList<Integer> item = new ArrayList<Integer>();
+        item.add(Integer.parseInt(request.getParameter("pid")));
+        item.add(Integer.parseInt(request.getParameter("quantity")));
+        cart.add(item);
+        session.setAttribute("cart", cart);
+
         PrintWriter out = response.getWriter();
 //        RequestDispatcher rd = request.getRequestDispatcher("history");
 //        try{
@@ -18,9 +29,11 @@ public class cart extends HttpServlet{
 //            out.println(e);
 //        }
         response.setContentType("text/html");
-        String css = request.getContextPath() + "/webfiles/main.css";
+
         try {
-            out.println("<form action='cart' method='post'>\n");
+            RequestDispatcher rd = request.getRequestDispatcher("item?pid=" + request.getParameter("pid"));
+            rd.include(request, response);
+            out.println("    <h4>Item Added to Cart</h4>\n");
         } catch (Exception e) {
             out.println(e);
         }
