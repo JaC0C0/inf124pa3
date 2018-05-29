ae3b8b81490e00228b1f6d750b5f6f84c0867433
Dennis Tian <JaCoCo@localhost.localdomain>
2018-05-29 10:29:27 -0700

    removed php

--- test/confirmation.java
+++ test/confirmation.java
@@ -14,7 +14,6 @@ public class confirmation extends HttpServlet {
                 "        <title>Foil Me, Daddy</title>\n" +
                 "        <!-- link to main stylesheet -->\n" +
                 "        <link rel=\"stylesheet\" type=\"text/css\" href=\"main.css\">\n" +
-                "        <?php require 'purchase.php';?>\n" +
                 "\n" +
                 "    </head>\n" +
                 "    <span id=\"insertHere\"></span>\n" +
