4a19675ca8d4842a369f9b3a8e3dc8f312b12100
Dennis Tian <JaCoCo@localhost.localdomain>
2018-05-29 06:55:00 -0700

    reverted css for recently viewed items

--- test/history.java
+++ test/history.java
@@ -75,7 +75,7 @@ public class history extends HttpServlet {
                         int inv = rs.getInt("inv");
                         int newpid = rs.getInt("pid");
                         out.println("   <tr class = 'itemBox'>" +
-                                "       <td class = 'recent'>" +
+                                "       <td class = 'picCol'>" +
                                 "           <a href='item?pid=" + newpid + "'>" +
                                 "           <img src='" + img + "' alt=" + name + " class='fill grow'>" +
                                 "           </a>" +
